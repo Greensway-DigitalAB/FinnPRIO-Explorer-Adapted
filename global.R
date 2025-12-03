e0458c6054cd0a3ed72b0a5dccc5a49cdcdfe382
Alejandro Ruete <aleruete@users.noreply.github.com>
2025-12-03 10:03:26 +0100

    add path to projekt

--- global.R
+++ global.R
@@ -15,16 +15,22 @@ library(gghighlight)
 library(treemapify)
 
 
-# source("functions.R")
 
-# The colors used in the Plot and in the Tables are from the palette of Ruokavirasto (2022).
+OS <- Sys.info()[['sysname']]
+if (OS  == "Linux") {
+  datapath <- "project-vol"
+  # datapath <- "/srv/shiny-server/data"
+  # datapath <- "/home/data" # (project-vol (FinnPRIO-Explorer Adapted))
+} else {
+  datapath <- "data/"
+}
 
 # Read data tables:
 # cleanfinnprioresults <- fread("data/cleanfinnprioresults.csv") # "cleanfinnprioresults.csv" contains FinnPRIO scores./It is used in tabs 1. Plot pests on a graph and 2. Show pests in data table/ The pest's names are used in tab 3. Compare pests by questions
 # pestquestions <- fread("data/pestquestions_est3.csv")          # "pestquestions_est3.csv" contains FinnPRIO assessments. Each pest is in a column!/ It is used in tab 3. Compare pests by questions
-hv <- fread("data/hv.csv")                                     # "hv.csv" contains FinnPRIO hypervolume scores./It is used in tab 4. Rank pests
+# hv <- fread("data/hv.csv")                                     # "hv.csv" contains FinnPRIO hypervolume scores./It is used in tab 4. Rank pests
 
-db_file <- list.files("data/", pattern = "FinnPrio_DB_", recursive = TRUE, full.names = TRUE)
+db_file <- list.files(datapath, pattern = "FinnPrio_DB_", recursive = TRUE, full.names = TRUE)
 #get the latest file if multiple
 if(length(db_file) > 1){
   db_file <- db_file[which.max(file.info(db_file)$mtime)]
